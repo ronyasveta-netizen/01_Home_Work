ef30dbd982a554f460fb16244ad8080b1c20dfd8
Ivan <ronya.sveta@gmail.com>
2026-04-13 22:52:28 +0300

    Add project files.

---
+++ 01_Home_Work_C++/01_Home_Work_C++.cpp
@@ -0,0 +1,42 @@
+﻿// 01_Home_Work_C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+
+    cout << "\t\t++ 1 ++\n\n";
+    cout << "\n \tCities are blooming,\n \tthe blue sky is clearing,\n \tThe sun is shining gold from above\n \tAnd above the houses our dream:\n \tYellow and blue flags.\n \t\tAvtor: Oleksandr Oles\n \t\t\t1917 year\n\n";
+    cout << "\t\t++ 2 ++\n\n";
+    cout << "\tEvery \n\t\tHunter \n\t\t\tWants \n\t\t\t\tto Know \n\t\t\t\t\t Where \n\t\t\t\t\t\tthe Pheasant \n\t\t\t\t\t\t\t     is Sitting\n\n";
+    cout << "\t\t++ 3 ++\n\n";
+    cout << R"(
+  .........................................................
+  :                                                       :
+  :            I will give a kitten as a gift.            :
+  :            breed:  Scottish Fold                      :
+  :       call number : (050)-609-88-39 after 7 p.m.      :
+  :                                                       :
+  .........................................................
+  |      |      |      |      |      |      |      |      |
+  | 050  | 050  | 050  | 050  | 050  | 050  | 050  | 050  |
+  | 609  | 609  | 609  | 609  | 609  | 609  | 609  | 609  |
+  |  88  |  88  |  88  |  88  |  88  |  88  |  88  |  88  |
+  |  39  |  39  |  39  |  39  |  39  |  39  |  39  |  39  |
+  |      |      |      |      |      |      |      |      |
+            
+    
+                
+                )";
+
+
+
+    system("pause");
+
+}
+
+
+
+
